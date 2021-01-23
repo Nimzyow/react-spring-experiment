8e99d3a2d7be06a30b8f20c868f13ea67a5353a1
Nimzyow <n_soufiani@hotmail.com>
2021-01-23 13:15:45 +0000

    first example of transition

--- src/Toggle.js
+++ src/Toggle.js
@@ -1,29 +1,23 @@
 import React, { useState } from 'react'
-import { useSpring, animated } from "react-spring"
+import { useTransition, animated } from "react-spring"
 
 const Toggle = () => {
     const [isToggled, setToggle] = useState(false);
-    const {color, y} = useSpring({
-        // opacity: isToggled ? 1 : 0,
-        color: isToggled ? "tomato" : "green",
-        // fontSize: isToggled ? "2rem" : "5em",
-        y: isToggled ? 0 : 1,
+    // with a transition we can mount and unmount a component from the dom
+    const transition = useTransition(isToggled, null, {
+        from: { opacity: 0 },
+        enter: { opacity: 1 },
+        leave: { opacity: 0 }
         
     })
 
-return (
+    return (
         <div>
-            <animated.h1 
-            style={{
-                transform: y.interpolate({
-                    range: [0, .25, .50, .75, 1],
-                    output: [0, -25, -50, -100, -50]
-                }).interpolate(y => (`translate3d(0,${y}px,0)`)),
-                color,
-                }}
-            >
-                Hello
-            </animated.h1>
+            {transition.map(
+                ({ item, key, props }) => (
+                item && <animated.h1 key={key} style={props}>Hello</animated.h1>
+            ))}
+            
             <button onClick={() => setToggle(!isToggled)}>
                 Toggle
             </button>

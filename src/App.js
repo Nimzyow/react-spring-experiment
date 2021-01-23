6bef5fd924149c2f3ee1ec15cb1e3a8cf3eca450
Nimzyow <n_soufiani@hotmail.com>
2021-01-23 11:42:32 +0000

    use spring hook to translate div

--- src/App.js
+++ src/App.js
@@ -1,17 +1,23 @@
-import React from 'react';
+import React, {useState} from 'react';
 import { useSpring, animated } from "react-spring"
 import Toggle from "./Toggle"
+import Nav from "./Nav"
 import logo from './logo.svg';
 import './App.css';
 
 const App = () => {
+  const [isNavOpen, setNavOpen] = useState(false)
+  
+  const navAnimation = useSpring({
+    transform: isNavOpen ? "translate3d(0,0,0) scale(1)" : "translate3d(100%, 0, 0) scale(0.6)"
+  })
 
   const fade = useSpring({
   from: {
       opacity: 0
     },
     opacity: 1
-  })
+  });
 
   console.log(fade)
 
@@ -19,7 +25,8 @@ const App = () => {
       <animated.div className="App" style={fade}>
         <header className="App-header">
           <img src={logo} className="logo" />
-          <button className="menu-button">Menu</button>
+          <button onClick={() => setNavOpen(!isNavOpen)} className="menu-button">Menu</button>
+          <Nav style={navAnimation}/>
         </header>
         <main>
           <Toggle />

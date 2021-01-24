d4545c6d3447d64dbd3c2362106b79041c8a5485
Nimzyow <n_soufiani@hotmail.com>
2021-01-24 21:11:33 +0000

    use chain example. chaining multiple animations together and staggering different animations

--- src/Boxes.js
+++ src/Boxes.js
@@ -1,19 +1,65 @@
-import React, {useState} from 'react'
-import {animated, useTrail} from "react-spring"
+import React, {useState, useRef} from 'react'
+import {animated, useTrail, useSpring, useChain, useTransition} from "react-spring"
 
-const items = [0.2,0.4,0.5,0.6,0.79,1]
+const items = [0.2,0.4,0.5,0.6,0.79,1, 12, 42, 67, 324, 346, 2213, 52, 11, 98]
 
 const Boxes = () => {
     const [on, toggle] = useState(false)
-    const trail = useTrail(items.length,{
-            opacity: on ? 0 : 1,
-            transform: on ? "scale(0.3)" : "scale(1)"
+
+    const springRef = useRef()
+    
+    const transitionRef = useRef()
+    
+    const { size } = useSpring({
+        ref: springRef,
+        from: {size: "20%"},
+        to: {size: on ? "100%" : "20%" }
+    })
+    //with trail
+    // const trail = useTrail(items.length,{
+    //         ref: transitionRef,
+    //        from: { opacity: on ? 1 : 0,
+    //         transform: "scale(0)"},
+    //        to: { opacity: on ? 1 : 0,
+    //         transform: on ? "scale(1)" : "scale(0)"}
+
+    //      })
+    const transition = useTransition(on ? items : [], item => item, {
+            ref: transitionRef,
+            trail: 400 / items.length,
+            from: { 
+                opacity: 0,
+                transform: "scale(0)"
+            },
+            enter: { 
+                opacity: 1,
+                transform: "scale(1)"
+            },
+            leave: { 
+                opacity: 0,
+                transform: "scale(0)"
+            },
+            
 
          })
+
+
+
+    useChain(on ? [springRef, transitionRef] : [transitionRef, springRef])
+
     return (
-        <div className="boxes-grid">
-            <button onClick={() => toggle(!on)}>Toggle</button>
-            {trail.map(animation => <animated.div className="box" style={animation}/> )}
+        <div className="full-height">
+        <animated.div 
+        style={{width: size, height: size}}
+        className="boxes-grid-two" 
+        onClick={() => toggle(!on)} >
+            {/* {trail.map(animation => 
+            <animated.div className="box-two" style={animation}/> 
+            )} */}
+            {transition.map(({item, key, props}) => 
+            <animated.div className="box-two" key={key} style={props}/> 
+            )}
+        </animated.div>
         </div>
     )
 }
